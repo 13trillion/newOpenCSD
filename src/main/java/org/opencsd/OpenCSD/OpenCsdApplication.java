c7880b1aec52cdaaacc1aed5dd1088b928eda769
Bilal Gutu <Bilalgutu13@gmail.com>
2022-10-19 16:52:39 -0400

    first

---
+++ src/main/java/org/opencsd/OpenCSD/OpenCsdApplication.java
@@ -0,0 +1,13 @@
+package org.opencsd.OpenCSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/opencsd/OpenCSD/OpenCsdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opencsd.OpenCSD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
